7baf0647057bc58ac4290bd663953c818a5b9240
tinshee <tinshee@sohu.com>
2015-07-17 15:00:53 +0800

    'app.js'

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,4 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('ease', ['ngResource', 'angular-md5','angular-underscore','ionic', 'angularFileUpload','ease.controllers', 'ease.services', 'ease.filters','ease.directives'])
 
     .run(function ($rootScope,$ionicPlatform,$stateParams) {
@@ -11,13 +6,12 @@ angular.module('ease', ['ngResource', 'angular-md5','angular-underscore','ionic'
         $rootScope.$stateParams = $stateParams;
 
         $ionicPlatform.ready(function () {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
+
             if (window.cordova && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             }
             if (window.StatusBar) {
-                // org.apache.cordova.statusbar required
+
                 StatusBar.styleDefault();
             }
         });
